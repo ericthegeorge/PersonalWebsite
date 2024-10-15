6de98dbf31b9ec6c8bc4cdac35aa03ca64df12ca
Eric George <erickurian@gmail.com>
2024-10-15 13:30:32 -0600

    rebasing onto main

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,31 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { MouseEvent } from "react";
-//requires items[] and heading string
+import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
 }
 function ListGroup({ items, heading }: Props) {
-  const handleClick = (event: MouseEvent) => console.log(event);
+  //hook allows to tap into builtin features of react
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // arr[0] variable (selected index)
+  // arr[1] updater function
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
