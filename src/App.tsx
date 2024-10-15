6e77e35df1b2f8b81027230cac3e5baf46f62eb5
Eric George <erickurian@gmail.com>
2024-10-15 13:46:41 -0600

    new alert component and passign html in interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   const handleSelectedItem = (item: string) => {
     console.log(item);
   };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectedItem}
-      />
+      <Alert>
+        Hello <span>another</span> world
+      </Alert>
     </div>
   );
 }
