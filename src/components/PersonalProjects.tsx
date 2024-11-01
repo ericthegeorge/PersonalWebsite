bf86f887ae2c874d80c22f849eaa09b06e5eb89b
Eric George <erickurian@gmail.com>
2024-11-01 00:05:04 -0600

    Added ray tracer to section

--- src/components/PersonalProjects.tsx
+++ src/components/PersonalProjects.tsx
@@ -1,19 +1,67 @@
 import React from "react";
-import { Grid2, Avatar, Card, CardContent, Typography } from "@mui/material";
+import {
+  Grid2,
+  Avatar,
+  Card,
+  CardContent,
+  Typography,
+  Box,
+  useTheme,
+} from "@mui/material";
 
-import pic from "./images/profileimage.jpeg";
+import rt1pic from "../assets/rt1pic.png";
+import rt2pic from "../assets/rt2pic.png";
+import rt3pic from "../assets/rt3pic.png";
+import rt4pic from "../assets/rt4pic.png";
+import RayTracerPP from "./RayTracerPP";
+
+interface subProps {
+  src: string;
+  alt: string;
+  caption: string;
+}
+const ImageWithCaption = ({ src, alt, caption }: subProps) => {
+  const theme = useTheme(); // Get the current theme
+
+  return (
+    <Box width={"65rem"}>
+      <img
+        src={src}
+        alt={alt}
+        style={{ width: "100%", height: "auto", borderRadius: "5rem" }}
+        width={"100 rem"}
+      />
+      <Typography
+        variant="body1"
+        display="block"
+        align="center"
+        sx={{
+          fontFamily: "Cormorant Garamond",
+          fontSize: 20,
+          fontStyle: "oblique",
+          marginTop: "0.1rem",
+          color: theme.palette.text.primary,
+        }}
+      >
+        {caption}
+      </Typography>
+    </Box>
+  );
+};
+
 function PersonalProjects() {
   return (
     <>
       <Grid2
         container
         spacing={2}
-        justifyContent="flex-end"
+        display={"flex"}
+        justifyContent={"center"}
         sx={{ backgroundColor: "transparent", paddingLeft: 10 }}
       >
         <Card
           sx={{
-            backgroundColor: "rgba(255, 255, 255, 0)",
+            backgroundColor: "#7e9ae6",
             backdropFilter: "blur(7px)",
             borderRadius: "10%",
             width: { xs: "90%", sm: "80%", md: "60%" },
@@ -21,32 +69,16 @@ function PersonalProjects() {
         >
           <CardContent>
             <Typography
-              variant="h5"
+              variant="h4"
               align="center"
-              sx={{ fontFamily: '"Open Sans", cursive, sans-serif' }}
+              sx={{ fontFamily: "Cormorant Garamond" }}
             >
               Personal Projects
             </Typography>
-            <Typography
-              variant="body1"
-              align="center"
-              sx={{
-                fontFamily: '"Playfair Display", cursive, sans-serif',
-              }}
-            >
-              My content here!
-            </Typography>
           </CardContent>
         </Card>
-        <Avatar
-          alt="Your Name"
-          src={pic}
-          sx={{
-            width: 200,
-            height: 200,
-          }}
-        />
       </Grid2>
+      <RayTracerPP />
     </>
   );
 }
